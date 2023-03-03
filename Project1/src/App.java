728df568b78291b5222e4885fe84f4f8ce3de6bc
Cj72001 <00038619@uca.edu.sv>
2023-03-02 23:37:10 -0600

    Add: LinkedList
    In the previous chapter, you learned about the ArrayList class. The LinkedList class is almost identical to the ArrayList.
    
    The LinkedList class is a collection which can contain many objects of the same type, just like the ArrayList.
    
    The LinkedList class has all of the same methods as the ArrayList class because they both implement the List interface. This means that you can add items, change items, remove items and clear the list in the same way.
    
    However, while the ArrayList class and the LinkedList class can be used in the same way, they are built very differently.
    
    How the ArrayList works
    The ArrayList class has a regular array inside it. When an element is added, it is placed into the array. If the array is not big enough, a new, larger array is created to replace the old one and the old one is removed.
    
    How the LinkedList works
    The LinkedList stores its items in "containers." The list has a link to the first container and each container has a link to the next container in the list. To add an element to the list, the element is placed into a new container and that container is linked to one of the other containers in the list.
    
    LinkedList Methods
    For many cases, the ArrayList is more efficient as it is common to need access to random items in the list, but the LinkedList provides several methods to do certain operations more efficiently like:
    
    addFirst()
    addLast()
    removeFirst()
    removeLast()
    getFirst()
    getLast()
    
    WHEN To Use
    Use an ArrayList for storing and accessing data, and LinkedList to manipulate data.
    
    Resume:
    -LinkedList

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,42 +1,46 @@
-import java.util.ArrayList; // import the ArrayList class
 import java.util.Collections; // Import the Collections class
+import java.util.LinkedList; // import the LinkedList class
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        //Java ArrayList: elements can be added and removed from an ArrayList whenever you want.
-        ArrayList<String> cars = new ArrayList<String>(); // Create an ArrayList object
+        //The LinkedList stores its items in "containers." The list has a link to the first container
+        //and each container has a link to the next container in the list.
+        LinkedList<String> cars = new LinkedList<String>(); 
 
         System.out.println(cars.size());
         
-        cars.add("Bolvo");
-        cars.add("Amber");
-        cars.add("Coush");
+        //USING LINKEDLIST METHODS TO ADD CARS IN ORDER OF COME:
+        cars.addLast("Bolvo"); //FIRST INCOME
+        cars.addLast("Amber");
+        cars.addLast("Coush"); //LAST INCOME
 
         System.out.println(cars.size());
 
         
-        System.out.println("First way to traverse the ArrayList by a loop"); 
+        System.out.println("First way to traverse the LinkedList by a loop"); 
         for(int i=0; i<cars.size(); i++){
             System.out.println(cars.get(i));
         }
 
-        System.out.println("Second way to traverse the ArrayList by a for-each loop"); 
+        System.out.println("Second way to traverse the LinkedList by a for-each loop"); 
         for(String c : cars){
             System.out.println(c);
         }
 
-        System.out.println("Third way to traverse the ArrayList by a for-each lambda");
+        System.out.println("Third way to traverse the LinkedList by a for-each lambda");
         cars.forEach(c->{
             System.out.println(c);
         });
 
-        //Sorting the ArrayList (it can be sorted numerically or alphabetically) using the Class Collections
+        //Sorting the LinkedList (it can be sorted numerically or alphabetically) using the Class Collections
         Collections.sort(cars);
         System.out.println("Sorted List of Cars:");
         cars.forEach(c->{
             System.out.println(c);
         });
+
+        
 
 }
 }
