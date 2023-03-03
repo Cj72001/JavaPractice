0dde74cd33b1b77ec284d82e055a84743d913b2e
Cj72001 <00038619@uca.edu.sv>
2023-03-03 00:33:07 -0600

    Add: HashSet
    Java HashSet
    A HashSet is a collection of items where every item is unique, and it is found in the java.util package.
    
    The HashSet class has many useful methods. For example, to add items to it, use the add() method.
    
    Check If an Item Exists
    To check whether an item exists in a HashSet, use the contains() method, even though some object is added twice it only appears once in the set because every item in a set has to be unique.
    
    Check If an Item Exists
    To check whether an item exists in a HashSet, use the contains() method.
    
    Remove an Item
    To remove an item, use the remove() method, To remove all items, use the clear() method.
    
    To find out how many items there are, use the size method.
    
    Loop Through a HashSet
    Loop through the items of an HashSet with a for-each loop.
    
    Other Types
    Items in an HashSet are actually objects. In the examples above, we created items (objects) of type "String". Remember that a String in Java is an object (not a primitive type). To use other types, such as int, you must specify an equivalent wrapper class: Integer. For other primitive types, use: Boolean for boolean, Character for char, Double for double, etc.
    
    Resume:
    -HashSet

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,74 +1,35 @@
 import java.util.Collections; // Import the Collections class
-import java.util.HashMap; // import the HashMap class
+import java.util.HashSet; // import the HashSet class
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        //A HashMap however, store items in "key/value" pairs, 
-        //and you can access them by an index of another type (e.g. a String).
+        // Create a HashSet object called people
+        HashSet<String> people = new HashSet<String>();
 
-        //Creating a HashMap object called capitalCities that will store String keys and String values
-        HashMap<String,String> capitalCities = new HashMap<String,String>(); 
 
-        System.out.println(capitalCities.size());
-        
-        // Add keys and values (Country, City)
-        capitalCities.put("England", "London");
-        capitalCities.put("Germany", "Berlin");
-        capitalCities.put("Norway", "Oslo");
-        capitalCities.put("USA", "Washington DC");
-        System.out.println(capitalCities);
-        System.out.println(capitalCities.size());
+        // Add String Objects (it can be Int, Char, etc)
+        people.add("Omar Rafael Flores Alas");
+        people.add("Omar Rafael Flores Alas"); //It will appear only one time, because it has to be unique
+        people.add("Erick Alexis Flores Alas");
+        people.add("Jose Omar Flores Jurado");
 
-        
-        // Print keys (countries)
-        System.out.println("countries: "+ capitalCities.keySet());
-
-        //Loop through the items of a HashMap with a for-each loop:
-        System.out.println("First way to traverse the HashMap by a loop for-each, printing keys:"); 
-        for(String i: capitalCities.keySet()){
-            System.out.println(i);
-        }
-
-        //Loop through the items of a HashMap with a for-each lambda.
-        System.out.println("Second way to traverse the HashMap by a for-each lambda, printing keys:"); 
-
-        capitalCities.keySet().forEach(country ->{
-            System.out.println(country);
+        people.forEach(p->{
+            System.out.println(p);
         });
 
-        //------
+        String deleteP = "Erick Alexis Flores Alas";
+        String newP = "Dolores Siomara Alas de Flores";
 
-        // Print values (capitals), this method can be used same as keySet()
-        System.out.println("countries: "+ capitalCities.values());
-
-
-        //Printing each key and its respective value
-        capitalCities.keySet().forEach(c ->{
-            System.out.println("The capital of: "+c+" is: "+capitalCities.get(c));
-        });
-
-
-        //-------------------------------------------------------------------------------------
-        // Keys and values in a HashMap are actually objects. In the examples above, we used objects 
-        // of type "String". Remember that a String in Java is an object (not a primitive type). To 
-        // use other types, such as int, you must specify an equivalent wrapper class: Integer. For 
-        // other primitive types, use: Boolean for boolean, Character for char, Double for double, etc.
-
-        // Create a HashMap object called people
-        HashMap<Integer, String> people = new HashMap<Integer, String>();
-
-
-        // Add keys and values (Name, DUI)
-        people.put(06200577,"Omar Rafael Flores Alas");
-        people.put(05302537,"Erick Alexis Flores Alas");
-        people.put(56122373,"Jose Omar Flores Jurado");
-
-        System.out.println("People and their DUI:");
-        for (Integer i : people.keySet()) {
-          System.out.println("key: " + i + " value: " + people.get(i));
+        if(people.contains(deleteP)){
+            people.remove(deleteP);
+            people.add(newP);
         }
 
+        System.out.println("Lista Actualizada:");
+        people.forEach(p->{
+            System.out.println(p);
+        });
 
     }
 }
