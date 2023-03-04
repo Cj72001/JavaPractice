bf213c5b325341cede620cd382da622888bae2aa
Cj72001 <00038619@uca.edu.sv>
2023-03-04 00:32:46 -0600

    Add: Iterator
    Java Iterator
    An Iterator is an object that can be used to loop through collections, like ArrayList and HashSet. It is called an "iterator" because "iterating" is the technical term for looping.
    
    To use an Iterator, you must import it from the java.util package.
    
    Getting an Iterator
    The iterator() method can be used to get an Iterator for any collection.
    
    Example:
       // Get the iterator
        Iterator<String> it = cars.iterator();
    
        // Print the first item
        System.out.println(it.next());
    
    Looping Through a Collection
    To loop through a collection, use the hasNext() and next() methods of the Iterator (it can be with a while)
    
    Removing Items from a Collection
    Iterators are designed to easily change the collections that they loop through. The remove() method can remove items from a collection while looping.
    
    Trying to remove items using a for loop or a for-each loop would not work correctly because the collection is changing size at the same time that the code is trying to loop.
    
    ITERATOR CAN BE USED FOR ARRAYLIST, LINKEDLIST, HASHSET
    
    ITERATOR CANT BE USED FOR HASHMAP
    
    Note: Trying to remove items using a for loop or a for-each loop would not work correctly because the collection is changing size at the same time that the code is trying to loop.
    
    Resume:
    -Iterator

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,12 +1,46 @@
+import java.util.ArrayList;
 import java.util.Collections; // Import the Collections class
+import java.util.HashMap;
 import java.util.HashSet; // import the HashSet class
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        //Java ArrayList: elements can be added and removed from an ArrayList whenever you want.
+        ArrayList<String> cars = new ArrayList<String>(); // Create an ArrayList object
+        cars.add("Bolvo");
+        cars.add("Amber");
+        cars.add("Coush");
+        //____________________________________________________________________________________________
+
+        //The LinkedList stores its items in "containers." The list has a link to the first container
+        //and each container has a link to the next container in the list.
+        LinkedList<String> laptops = new LinkedList<String>(); 
+        
+        //USING LINKEDLIST METHODS TO ADD CARS IN ORDER OF COME:
+        laptops.addLast("HP"); //FIRST INCOME
+        laptops.addLast("DELL");
+        laptops.addLast("Apple"); //LAST INCOME
+
+        //____________________________________________________________________________________________
+
+        //Creating a HashMap object called capitalCities that will store String keys and String values
+        HashMap<String,String> capitalCities = new HashMap<String,String>(); 
+
+        System.out.println(capitalCities.size());
+        
+        // Add keys and values (Country, City)
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+
+        //____________________________________________________________________________________________
+
         // Create a HashSet object called people
         HashSet<String> people = new HashSet<String>();
-
 
         // Add String Objects (it can be Int, Char, etc)
         people.add("Omar Rafael Flores Alas");
@@ -14,22 +48,51 @@ public class App {
         people.add("Erick Alexis Flores Alas");
         people.add("Jose Omar Flores Jurado");
 
-        people.forEach(p->{
-            System.out.println(p);
-        });
+        //____________________________________________________________________________________________
 
-        String deleteP = "Erick Alexis Flores Alas";
-        String newP = "Dolores Siomara Alas de Flores";
 
-        if(people.contains(deleteP)){
-            people.remove(deleteP);
-            people.add(newP);
+
+
+
+
+
+
+
+        //CREATE AN ITERATOR FOR EACH ONE (ARRAYLIST, LINKEDLIST, HASHMAP, HASHSET)
+        //ArrayList:
+        Iterator i1 = cars.iterator();
+
+        while(i1.hasNext()){
+            
+            String comp = (String) i1.next();
+
+            if(comp.equals("Bolvo")){
+                i1.remove();
+            }
+            else{
+                System.out.println(comp);
+            }
         }
 
-        System.out.println("Lista Actualizada:");
-        people.forEach(p->{
-            System.out.println(p);
-        });
+
+        i1 = laptops.iterator();
+
+        while(i1.hasNext()){
+            System.out.println(i1.next());
+        }
+
+        //ITERATOR CANT BE USED FOR HASHMAP
+        //WRONG -> i1 = capitalCities.iterator();
+
+        i1 = people.iterator();
+
+        while(i1.hasNext()){
+            System.out.println(i1.next());
+        }
+
+
+
+
 
     }
 }
