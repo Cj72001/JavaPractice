f94ceb6c11cd2fb5f77b64e1f13ab4a15e6604b4
Cj72001 <00038619@uca.edu.sv>
2023-03-05 18:05:10 -0600

    Add: Wrapper Classes
    Java Wrapper Classes
    Wrapper classes provide a way to use primitive data types (int, boolean, etc..) as objects.
    
    Sometimes you must use wrapper classes, for example when working with Collection objects, such as ArrayList, where primitive types cannot be used (the list can only store objects).
    
    Creating Wrapper Objects
    To create a wrapper object, use the wrapper class instead of the primitive type. To get the value, you can just print the object.
    
    Since you're now working with objects, you can use certain methods to get information about the specific object.
    
    Another useful method is the toString() method, which is used to convert wrapper objects to strings, in that way we can use String methods too.
    
    Resume:
    -Wrapper Classes

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,98 +1,43 @@
-import java.util.ArrayList;
-import java.util.Collections; // Import the Collections class
-import java.util.HashMap;
-import java.util.HashSet; // import the HashSet class
-import java.util.Iterator;
-import java.util.LinkedList;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
+    
+        //Wrapper classes provide a way to use primitive data types (int, boolean, etc..) as objects.
 
-        //Java ArrayList: elements can be added and removed from an ArrayList whenever you want.
-        ArrayList<String> cars = new ArrayList<String>(); // Create an ArrayList object
-        cars.add("Bolvo");
-        cars.add("Amber");
-        cars.add("Coush");
-        //____________________________________________________________________________________________
+        //Using primitive data types:
+        int n1 = 764;
 
-        //The LinkedList stores its items in "containers." The list has a link to the first container
-        //and each container has a link to the next container in the list.
-        LinkedList<String> laptops = new LinkedList<String>(); 
-        
-        //USING LINKEDLIST METHODS TO ADD CARS IN ORDER OF COME:
-        laptops.addLast("HP"); //FIRST INCOME
-        laptops.addLast("DELL");
-        laptops.addLast("Apple"); //LAST INCOME
-
-        //____________________________________________________________________________________________
-
-        //Creating a HashMap object called capitalCities that will store String keys and String values
-        HashMap<String,String> capitalCities = new HashMap<String,String>(); 
-
-        System.out.println(capitalCities.size());
-        
-        // Add keys and values (Country, City)
-        capitalCities.put("England", "London");
-        capitalCities.put("Germany", "Berlin");
-        capitalCities.put("Norway", "Oslo");
-        capitalCities.put("USA", "Washington DC");
-
-        //____________________________________________________________________________________________
-
-        // Create a HashSet object called people
-        HashSet<String> people = new HashSet<String>();
-
-        // Add String Objects (it can be Int, Char, etc)
-        people.add("Omar Rafael Flores Alas");
-        people.add("Omar Rafael Flores Alas"); //It will appear only one time, because it has to be unique
-        people.add("Erick Alexis Flores Alas");
-        people.add("Jose Omar Flores Jurado");
-
-        //____________________________________________________________________________________________
+        //Using primitive data types as objects, by Warapper classes:
+        Integer n2 = 229;
 
 
 
+        System.out.println("As numbers: "+n1+", "+n2);
+        System.out.println("As numbers n1 + n2: "+ (n1+n2));
 
 
+        String n1Str = String.valueOf(n1);
+        String n2Str = n2.toString();
+
+        System.out.println("As strings: "+n1Str+", "+n2Str);
 
 
+        //Printing each number of full number of n1 ^ n2:
 
+        int n3 = 0;
 
-        //CREATE AN ITERATOR FOR EACH ONE (ARRAYLIST, LINKEDLIST, HASHMAP, HASHSET)
-        //ArrayList:
-        Iterator i1 = cars.iterator();
-
-        while(i1.hasNext()){
-            
-            String comp = (String) i1.next();
-
-            if(comp.equals("Bolvo")){
-                i1.remove();
-            }
-            else{
-                System.out.println(comp);
-            }
+        for(int i =0; i<n1Str.length(); i++){
+            n3 = Character.getNumericValue(n1Str.charAt(i));
+            System.out.println(n3);
         }
 
+        System.out.println("--------");
 
-        i1 = laptops.iterator();
-
-        while(i1.hasNext()){
-            System.out.println(i1.next());
+        for(int i =0; i<n2Str.length(); i++){
+            n3 = Character.getNumericValue(n2Str.charAt(i));
+            System.out.println(n3);
         }
-
-        //ITERATOR CANT BE USED FOR HASHMAP
-        //WRONG -> i1 = capitalCities.iterator();
-
-        i1 = people.iterator();
-
-        while(i1.hasNext()){
-            System.out.println(i1.next());
-        }
-
-
-
-
 
     }
 }
