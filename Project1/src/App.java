3a261a0507e6b53db6ddb6823e1d2c021cdcb7d8
Cj72001 <00038619@uca.edu.sv>
2023-03-09 00:34:03 -0600

    Add: RegEx
    What is a Regular Expression?
    A regular expression is a sequence of characters that forms a search pattern. When you search for data in a text, you can use this search pattern to describe what you are searching for.
    
    A regular expression can be a single character, or a more complicated pattern.
    
    Regular expressions can be used to perform all types of text search and text replace operations.
    
    Java does not have a built-in Regular Expression class, but we can import the java.util.regex package to work with regular expressions. The package includes the following classes:
    
    ~Pattern Class - Defines a pattern (to be used in a search)
    ~Matcher Class - Used to search for the pattern
    ~PatternSyntaxException Class - Indicates syntax error in a regular expression pattern.
    
    Example Explained
    In this example, The word "w3schools" is being searched for in a sentence.
    
    First, the pattern is created using the Pattern.compile() method. The first parameter indicates which pattern is being searched for and the second parameter has a flag to indicates that the search should be case-insensitive. The second parameter is optional.
    
    The matcher() method is used to search for the pattern in a string. It returns a Matcher object which contains information about the search that was performed.
    
    The find() method returns true if the pattern was found in the string and false if it was not found.
    
    Flags
    Flags in the compile() method change how the search is performed. Here are a few of them:
    
    Pattern.CASE_INSENSITIVE - The case of letters will be ignored when performing a search.
    Pattern.LITERAL - Special characters in the pattern will not have any special meaning and will be treated as ordinary characters when performing a search.
    Pattern.UNICODE_CASE - Use it together with the CASE_INSENSITIVE flag to also ignore the case of letters outside of the English alphabet
    Regular Expression Patterns
    The first parameter of the Pattern.compile() method is the pattern. It describes what is being searched for.
    
    Brackets are used to find a range of characters:
    [abc]	Find one character from the options between the brackets
    [^abc]	Find one character NOT between the brackets
    [0-9]	Find one character from the range 0 to 9
    
    Metacharacters
    Metacharacters are characters with a special meaning:
    
    |	Find a match for any one of the patterns separated by | as in: cat|dog|fish
    .	Find just one instance of any character
    ^	Finds a match as the beginning of a string as in: ^Hello
    $	Finds a match at the end of the string as in: World$
    \d	Find a digit
    \s	Find a whitespace character
    \b	Find a match at the beginning of a word like this: \bWORD, or at the end of a word like this: WORD\b
    \uxxxx	Find the Unicode character specified by the hexadecimal number xxxx
    
    Quantifiers
    Quantifiers define quantities
    n+	Matches any string that contains at least one n
    n*	Matches any string that contains zero or more occurrences of n
    n?	Matches any string that contains zero or one occurrences of n
    n{x}	Matches any string that contains a sequence of X n's
    n{x,y}	Matches any string that contains a sequence of X to Y n's
    n{x,}	Matches any string that contains a sequence of at least X n's

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,62 +1,21 @@
-import java.util.Scanner;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) throws Exception {
     
-        //The try statement allows you to define a block of code to be tested for errors while it is being executed.
-        //The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
-        
+        String buscar = "w3schools";
+        Pattern pattern = Pattern.compile(buscar, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("Visit W3Schools!");
 
-        //This will generate an error, because myNumbers[10] does not exist.
-        //if an error occurs, we can use try...catch to catch the error and execute some code to handle it
-        try {
-            int[] myNumbers = {1, 2, 3};
-            System.out.println(myNumbers[10]);
-          } catch (Exception e) {
-            System.out.println("Array not found");
-          }
+        boolean matchFound = matcher.find();
 
-        
-        //The finally statement lets you execute code, after try...catch, regardless of the result
-        
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter first number: ");
-        int n1 = sc.nextInt();
-
-        System.out.println("Enter second number: ");
-        int n2 = sc.nextInt();
-
-        int res = 0;
-        
-        try {
-
-            res = n1/n2;
-
-          } catch (Exception e) {
-
-            System.out.println("Something were wrong: "+e);
-            System.out.println("we going to use the same first number...");
-            res = n1/n1;
-            
-          } finally {
-            System.out.println("The result is: "+res);
-          }
-
-
-
-          //The throw statement allows you to create a custom error
-          checkAge(15); // Set age to 15 (which is below 18...)
-
-
-    }
-
-    static void checkAge(int age) {
-        if (age < 18) {
-          throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        if(matchFound) {
+        System.out.println("Match found");
+        } else {
+        System.out.println("Match not found");
         }
-        else {
-          System.out.println("Access granted - You are old enough!");
-        }
-      }
+}
+
 }
