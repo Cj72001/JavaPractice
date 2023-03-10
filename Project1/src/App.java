36118632a7d6382227963c1d9d68805176ff3e87
Omar Flores <00038619@uca.edu.sv>
2023-03-10 17:20:47 -0600

    Add: Other way to search a pattern

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,4 +1,5 @@
 
+import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,6 +7,7 @@ public class App {
     public static void main(String[] args) throws Exception {
     
         String buscar = "w3schools";
+
         Pattern pattern = Pattern.compile(buscar, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher("Visit W3Schools!");
 
@@ -16,6 +18,18 @@ public class App {
         } else {
         System.out.println("Match not found");
         }
+
+
+        //Other way to found a pettern:
+        String comparacion = "Hello and wellcome to W3Schools";
+        
+        if(comparacion.toLowerCase().contains(buscar.toLowerCase())){
+            System.out.println("FOUND");
+        }
+        else{
+            System.out.println("NOT FOUND");
+        }
+        
 }
 
 }
