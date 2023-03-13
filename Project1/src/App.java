5efe9b60f9e6a1f4415cdc105d5bd143572ff820
Omar Flores <00038619@uca.edu.sv>
2023-03-12 19:40:20 -0600

    Add: Java Treads
    Java Threads
    Threads allows a program to operate more efficiently by doing multiple things at the same time.
    
    Threads can be used to perform complicated tasks in the background without interrupting the main program.
    
    Creating a Thread
    There are two ways to create a thread.
    
    It can be created by extending the Thread class and overriding its run() method.
    
    Another way to create a thread is to implement the Runnable interface.
    
    Running Threads
    If the class extends the Thread class, the thread can be run by creating an instance of the class and call its start() method.
    
    public class Main implements Runnable {
      public static void main(String[] args) {
        Main obj = new Main();
        Thread thread = new Thread(obj);
        thread.start();
        System.out.println("This code is outside of the thread");
      }
      public void run() {
        System.out.println("This code is running in a thread");
      }
    }
    
    Differences between "extending" and "implementing" Threads
    
    The major difference is that when a class extends the Thread class, you cannot extend any other class, but by implementing the Runnable interface, it is possible to extend from another class as well, like: class MyClass extends OtherClass implements Runnable.
    
    Concurrency Problems
    Because threads run at the same time as other parts of the program, there is no way to know in which order the code will run. When the threads and main program are reading and writing the same variables, the values are unpredictable. The problems that result from this are called concurrency problems.
    
    To avoid concurrency problems, it is best to share as few attributes between threads as possible. If attributes need to be shared, one possible solution is to use the isAlive() method of the thread to check whether the thread has finished running before using any attributes that the thread can change.
    
    Use isAlive() to prevent concurrency problems.
    
    Example:
    public class Main extends Thread {
      public static int amount = 0;
    
      public static void main(String[] args) {
        Main thread = new Main();
        thread.start();
        // Wait for the thread to finish
        while(thread.isAlive()) {
        System.out.println("Waiting...");
      }
      // Update amount and print its value
      System.out.println("Main: " + amount);
      amount++;
      System.out.println("Main: " + amount);
      }
      public void run() {
        amount++;
      }
    }
    
    Resume:
    -Java Threads

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,35 +1,46 @@
 
-import java.util.LinkedList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class App {
+//There are two ways to create a thread.
+//Another way to create a thread is to implement the Runnable interface
+//It can be created by extending the Thread class and overriding its run() method:
+public class App extends Thread{
+
+    //Attribute that belongs to the App class:
+    public static int var = 0;
+
     public static void main(String[] args) throws Exception {
-    
-        String buscar = "w3schools";
+    //Running Threads
+    //If the class extends the Thread class, the thread can be run by creating 
+    //an instance of the class and call its start() method
 
-        Pattern pattern = Pattern.compile(buscar, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher("Visit W3Schools!");
+    App thread = new App();
 
-        boolean matchFound = matcher.find();
+    //This method come from Thread class
+    thread.start();
 
-        if(matchFound) {
-        System.out.println("Match found");
-        } else {
-        System.out.println("Match not found");
-        }
+    System.out.println(var);
 
+    //All after this line is out of the thread, so to prevent 
+    //concurrency problems we use isAlive() method of thread object
+    //for waiting the end of the thread that is running at that moment
+    while(thread.isAlive()){
+        System.out.println("Waiting...");
+    }
 
-        //Other way to found a pettern:
-        String comparacion = "Hello and wellcome to W3Schools";
+    //the thread finished
+    var++;
+    System.out.println(var);
         
-        if(comparacion.toLowerCase().contains(buscar.toLowerCase())){
-            System.out.println("FOUND");
-        }
-        else{
-            System.out.println("NOT FOUND");
-        }
         
 }
+
+//This method is override from "Thread" class
+    @Override
+    public void run() {
+        System.out.println("This code is running in a thread");
+        var++;
+    }
+    
+
 
 }
