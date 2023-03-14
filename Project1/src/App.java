9c427e47a76b23f944d51ed5066d9312f563535f
Omar Flores <00038619@uca.edu.sv>
2023-03-14 16:25:43 -0600

    Add: Java files
    File handling is an important part of any application.
    
    Java has several methods for creating, reading, updating, and deleting files.
    
    Java File Handling
    The File class from the java.io package, allows us to work with files.
    
    To use the File class, create an object of the class, and specify the filename or directory name:
    
    import java.io.File;  // Import the File class
    
    File myObj = new File("filename.txt"); // Specify the filename
    
    The File class has many useful methods for creating and getting information about files. For example:
    
    Method	Type	Description
    canRead()	Boolean	Tests whether the file is readable or not
    canWrite()	Boolean	Tests whether the file is writable or not
    createNewFile()	Boolean	Creates an empty file
    delete()	Boolean	Deletes a file
    exists()	Boolean	Tests whether the file exists
    getName()	String	Returns the name of the file
    getAbsolutePath()	String	Returns the absolute pathname of the file
    length()	Long	Returns the size of the file in bytes
    list()	String[]	Returns an array of the files in the directory
    mkdir()	Boolean	Creates a directory

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,52 +1,17 @@
-import java.util.LinkedList;
-import java.util.function.Consumer;
+import java.io.File;  // Import the File class
 
-//There are two ways to create a thread.
-//Another way to create a thread is to implement the Runnable interface
-//It can be created by extending the Thread class and overriding its run() method:
+// Java File Handling
+// The File class from the java.io package, allows us to work with files.
+
+// To use the File class, create an object of the class, and specify the filename or directory name:
+
 public class App{
 
 
     public static void main(String[] args) throws Exception {
         
-        LinkedList l = new LinkedList<Integer>();
+        File myObj = new File("filename.txt"); // Specify the filename
 
-        for(int i=1; i<=5; i++){
-            l.add(i);
-        }
-
-        l.forEach(n->{
-            if((Integer)n!=4){
-                System.out.println(n);
-            }
-        });
-
-        System.out.println("_____________________");
-
-    //Lambda expressions can be stored in variables if the variable's type 
-    //is an interface which has only one method. The lambda expression should have the
-    //same number of parameters and the same return type as that method
-
-    Consumer<Integer> m = (n) ->{System.out.println(n);};
-
-    l.forEach(m);
-
-//To use a lambda expression in a method, the method should have a parameter with a 
-//single-method interface as its type. Calling the interface's method will run the lambda expression
-
-StringFunction changeStr = (s -> s = "New str");
-StringFunction addAsk = (s) -> s + "?";
-
-//sending the lambda methods above as a parameter:
-//but the methods will named as the name of the method of the interface
-printFormatted("Hello", changeStr);
-printFormatted("Hello", addAsk);
         
 }
-
-public static void printFormatted(String str, StringFunction format) {
-    String result = format.run(str);
-    System.out.println(result);
-  }
-
 }
--- Project1/src/StringFunction.java
+++
@@ -1,4 +0,0 @@
-public interface StringFunction {
-    
-    String run(String str);
-}
