248744a68354cd1ba44959174fc1255d1266cf07
Omar Flores <00038619@uca.edu.sv>
2023-03-14 17:02:36 -0600

    Add: Java create and write to files
    Create a File
    To create a file in Java, you can use the createNewFile() method. This method returns a boolean value: true if the file was successfully created, and false if the file already exists. Note that the method is enclosed in a try...catch block. This is necessary because it throws an IOException if an error occurs (if the file cannot be created for some reason).
    
    To create a file in a specific directory (requires permission), specify the path of the file and use double backslashes to escape the "\" character (for Windows). On Mac and Linux you can just write the path, like: /Users/name/filename.txt.
    
    Example:
    File myObj = new File("C:\\Users\\MyName\\filename.txt");
    
    Write To a File
    In the following example, we use the FileWriter class together with its write() method to write some text to the file we created in the example above. Note that when you are done writing to the file, you should close it with the close() method.

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,4 +1,6 @@
 import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.io.IOException;
 
 // Java File Handling
 // The File class from the java.io package, allows us to work with files.
@@ -9,8 +11,48 @@ public class App{
 
 
     public static void main(String[] args) throws Exception {
-        
-        File myObj = new File("filename.txt"); // Specify the filename
+
+        //-To create a file in a specific directory: 
+        //File myObj = new File("C:\\Users\\MyName\\filename.txt");
+
+        //-To create a file in a specific directory of this project:
+        //File myObj = new File("Directory\\filename.txt");
+
+        String filePath = "java files\\filename.txt";
+
+        try { 
+            //Creating a file in an specific directory of this project, in this case "java files"
+            File myObj = new File(filePath);
+
+            if (myObj.createNewFile()) {
+              System.out.println("File created: " + myObj.getName());
+            } else {
+              System.out.println("File already exists.");
+            }
+
+          } catch (Exception e) {
+
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+
+
+        //   In the following example, we use the FileWriter class together with its write() method
+        //   to write some text to the file we created in the example above. Note that when you are
+        //   done writing to the file, you should close it with the close() method
+
+          try {
+            FileWriter myWriter = new FileWriter("java files\\filename.txt");
+
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+
+            System.out.println("Successfully wrote to the file.");
+
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
 
         
 }
