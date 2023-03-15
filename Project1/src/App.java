e5d48d65d214d013e3407f6a4f910d996dfce49f
Omar Flores <00038619@uca.edu.sv>
2023-03-15 16:53:35 -0600

    Add: Delete files
    Delete a File
    To delete a file in Java, use the delete() method:
    
    File myObj = new File("filename.txt");
        if (myObj.delete()) {
          System.out.println("Deleted the file: " + myObj.getName());
        } else {
          System.out.println("Failed to delete the file.");
        }
    
    Delete a Folder
    You can also delete a folder. However, it must be empty:
    
    File myObj = new File("C:\\Users\\MyName\\Test");
        if (myObj.delete()) {
          System.out.println("Deleted the folder: " + myObj.getName());
        } else {
          System.out.println("Failed to delete the folder.");
        }
    
    Resume:
    -Java Files, Create/Write Files, Read Files
    -Java Delete Files.

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,6 +1,5 @@
 import java.io.File;  // Import the File class
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
 // Java File Handling
@@ -19,7 +18,8 @@ public class App{
 
             File myObj = new File("java files\\filename.txt");
 
-            //The "myReader" Scanner object is created with the file "myObj" as a parameter
+            if(myObj.exists()){
+              //The "myReader" Scanner object is created with the file "myObj" as a parameter
             Scanner myReader = new Scanner(myObj);
 
             //So the Scanner object "myReader" has the Scanner methods as "hasNextLine()"
@@ -29,7 +29,25 @@ public class App{
             }
 
             myReader.close();
-            
+            }
+            else{
+              //Creating a new file if it not exists
+              myObj.createNewFile();
+              System.out.println("New file created succesfully");
+
+              try{
+                //After creating new file, writing in it:
+                FileWriter myWritter = new FileWriter(myObj);
+
+                myWritter.write("Este en un nuevo archivo");
+                myWritter.close();
+              }catch(Exception e){
+                System.out.println("A problem ocurred");
+                e.printStackTrace();
+              }
+              
+            }
+
           } catch (Exception e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -50,7 +68,21 @@ public class App{
           else{
             System.out.println("The file does not exist");
           }
-            
+
+          System.out.println("______________________________________________");
+          Scanner sc = new Scanner(System.in);
+
+          System.out.println("Would you like to delete the recently created file?: 's' or 'n' ");
+          String res = sc.nextLine();
+
+          if(res.equals("s")){
+            //Deleting the file created recently:
+            if (myObj.delete()) { 
+              System.out.println("Deleted the file: " + myObj.getName());
+            } else {
+              System.out.println("Failed to delete the file.");
+            } 
+          }
 
         
 }
